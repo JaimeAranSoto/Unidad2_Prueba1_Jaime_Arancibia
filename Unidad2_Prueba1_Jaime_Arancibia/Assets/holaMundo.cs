ba4fa310b3f5a544acd642a17cda449ae3e5b6d1
StivArts <jaimearansoto@gmail.com>
2017-10-30 17:34:27 -0300

    Se añade feature que cambia el mensaje dependiendo de la hora.

--- Unidad2_Prueba1_Jaime_Arancibia/Assets/holaMundo.cs
+++ Unidad2_Prueba1_Jaime_Arancibia/Assets/holaMundo.cs
@@ -1,20 +1,38 @@
-public class HiWorld : MonoBehaviour {
-  
- +	private string message;
- +	
-      public Text text;
-  
-  	// Use this for initialization
- @@ -16,4 +18,12 @@ public class HiWorld : MonoBehaviour {
-  	void Update () {
-  		
-  	}
- +	
- +	public string HolaMundo(){
- +		//Me voy a tomar un cafe, que el hola mundo entregue la hora actual y 
- +		//según la hora, diga buenos días mundo, 
- +		//buenas tarde o buenas noches
- +		message="";
- +		return message;
- +	}
-  }
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class holaMundo : MonoBehaviour
+{
+
+    private string message;
+
+    private Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+
+        InvokeRepeating("setHora", 0, 10);
+    }
+    void Update()
+    {
+
+    }
+    void setHora()
+    {
+        text.text = HolaMundo();
+    }
+    public string HolaMundo()
+    {
+        string message = "";
+        int hora = DateTime.Now.Hour;
+        if (hora >= 6 && hora < 12)
+            message = "buenos dias mundo";
+        if (hora >= 12 && hora < 22)
+            message = "buenas tardes mundo";
+        if (hora >= 22 || hora < 6)
+            message = "buenas noches mundo";
+        return message;
+    }
+}
